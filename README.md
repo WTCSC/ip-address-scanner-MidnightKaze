9250dae63e8456735e6be9d0f8135718dde24cc8
Caitlyn Nguyen <winternguyen0@gmail.com>
2025-01-28 13:14:58 -0700

    Uh... testing will be completed later :D

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/cYbEVSqo)
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=17891882)
+
+# Ip Address Scanner
+Have you ever wanted to scan the entirety of an network from one simple CIRD address? The answer to that is probably, "I've never ever wanted to do that."
+
+## Set up and Installation
+
+To get started, clone `scanner.py` using whatever method you'd like. After that setting up and installing is as simple as ensuring the python file is inside of your working or current directory.
--- scanner.py
+++ scanner.py
@@ -1,7 +1,5 @@
 #!/bin/python3
 
-# this will be better documented later on lmao
-
 # will remove the unneeded ones later lol
 import subprocess
 import math
@@ -11,51 +9,61 @@ import platform
 import time
 
 def ping_that_ip(ip):
-    # b/c window and linux use differnt flags in ping, we'll account for both
+    # B/C Windows and Linux use differnt flags for a number ping, we'll account for both
     flag = "-n" if platform.system().lower == "windows" else "-c"
     command = ['ping', flag, '1', str(ip)]
     
     try:
-        # runs the command while also keep track of the times
+        # Runs the command while also keep track of the times
         start = time.time()
         output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, timeout=5)
         end = time.time()
 
-        # if the command was ran succesfully (exit 0) then it will continue on by spitting out the time
+        # If the command was ran succesfully (exit 0) then it will continue on by giving out the time
         if output.returncode == 0:
             response_time = math.floor((end-start)*1000) # Returns the number of milliseconds, floored
             return "UP", response_time, None
         
-        # else wise it will return an error
+        # Else wise it will return an error
         else:
             return "DOWN", None, output.stderr
     
-    # if it was a timeout error the follow will proceed (timeout=5 gives the code 5 seconds to exicute before it will teminate)
+    # If it was a timeout error the follow will proceed (timeout=5 gives the code 5 seconds to respond before it will terminate)
     except subprocess.TimeoutExpired:
         return "DOWN", None, "REQUEST TIMED OUT."
     
-    # if anything gets by it'll spit this out
+    # If anything gets by it'll give this instead
     except Exception as e:
         return "DOWN", None, str(e)
 
 # LETS THROW IT ALL TOGETHER
 def main():
+    # The CIDR Address is the only argument passed through
     cidr = sys.argv[1]
 
-    try: # <<< could use if not if you wanted to ig
+    # This will basically just check that a valid IP Address and will return the respective network for the address
+    try:
         netwrk = ipaddress.ip_network(cidr, strict=True)
+    
+    # Should strict turn to False, it will return the ValueError and return the error message to the user
     except ValueError as e:
         print("Please provide a valid CIRD notation network range. Double check and try again.")
         sys.exit(1)
 
+    # Just some messages for the user
     print(f"Scanning network {cidr}...")
     print(f"Total number of found hosts: {netwrk.num_addresses - 2} (not including network and broadcast hosts)")
 
+    # Begins to iterate through each ip found in the range
     for ip in netwrk.hosts():
+        # ping_that_ip returns will return three values
         status, response_time, error = ping_that_ip(ip)
 
+        # If the host is up it will return this
         if status == "UP":
             print(f"{ip}: UP [Response Time: {response_time} ms]")
+        
+        # If the host is down it will return this instead
         else:
             print(f"{ip}: DOWN [Error: {error}]")
 
